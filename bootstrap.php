ada753184b439592e8297254281630738d99bf37
Sergio Vilar <sergiovilar.r@gmail.com>
2015-09-13 22:46:08 -0300

    Define APPLICATION_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 
 use Alter\Option\Loader\Option;
-if(function_exists('get_bloginfo')) new Option(); // Only loads in WordPress environment
+if(function_exists('get_bloginfo')) { // Only loads in WordPress environment
+  if(!defined('APPLICATION_PATH')) define('APPLICATION_PATH', \get_template_directory());
+  new Option();
+}
